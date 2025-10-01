4b6151d42893016bb77a49c7caf17af9f652b17e
tetianapavlova <pavlova0409@gmail.com>
2025-09-30 23:44:00 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  /* Footer */
+
   const footer = document.createElement("footer")
   document.body.appendChild(footer)
 
@@ -11,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
   footer.appendChild(copyright)
 
   /*Skills*/
+
   const skills = ["HTML", "CSS", "JavaScript", "GitHub"]
   const skillsSection = document.querySelector("#Skills")
   const skillsList = skillsSection.querySelector("ul")
@@ -20,4 +23,38 @@ document.addEventListener("DOMContentLoaded", function () {
     skill.innerText = skills[i]
     skillsList.appendChild(skill)
   }
+
+  /* Messages */
+
+  const messageForm = document.forms["leave_message"]
+
+  messageForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+
+    const usersName = event.target.usersName.value
+    const usersEmail = event.target.usersEmail.value
+    const usersMessage = event.target.usersMessage.value
+
+    console.log(usersName, usersEmail, usersMessage)
+
+    const messageSection = document.querySelector("#messages")
+    const messageList = messageSection.querySelector("ul")
+
+    const newMessage = document.createElement("li")
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>: <span>${usersMessage}</span>`
+
+    const removeButton = document.createElement("button")
+    removeButton.type = "button"
+    removeButton.innerText = "remove"
+
+    removeButton.addEventListener("click", function () {
+      const entry = removeButton.parentNode
+      entry.remove()
+    })
+
+    newMessage.appendChild(removeButton)
+    messageList.appendChild(newMessage)
+
+    messageForm.reset()
+  })
 })
